07b93b63466765cf2c780109d6ab2f9879a19f17
Kirill Bulatov <kirill@zed.dev>
2024-03-03 15:53:10 +0200

    Draft showing git diff hunks in the editor

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -215,6 +215,7 @@ pub fn render_parsed_markdown(
 pub(crate) enum InlayId {
     Suggestion(usize),
     Hint(usize),
+    GitHunk(usize, DiffHunkStatus),
 }
 
 impl InlayId {
@@ -222,10 +223,12 @@ impl InlayId {
         match self {
             Self::Suggestion(id) => *id,
             Self::Hint(id) => *id,
+            Self::GitHunk(id, _) => *id,
         }
     }
 }
 
+enum GitRowHighlight {}
 enum DocumentHighlightRead {}
 enum DocumentHighlightWrite {}
 enum InputComposition {}
@@ -452,6 +455,7 @@ pub struct Editor {
     active_inline_completion: Option<Inlay>,
     show_inline_completions: bool,
     inlay_hint_cache: InlayHintCache,
+    git_inlays: HashMap<InlayId, Inlay>,
     next_inlay_id: usize,
     _subscriptions: Vec<Subscription>,
     pixel_position_of_newest_cursor: Option<gpui::Point<Pixels>>,
@@ -1500,6 +1504,7 @@ impl Editor {
             inline_completion_provider: None,
             active_inline_completion: None,
             inlay_hint_cache: InlayHintCache::new(inlay_hint_settings),
+            git_inlays: HashMap::default(),
             gutter_hovered: false,
             pixel_position_of_newest_cursor: None,
             gutter_width: Default::default(),
@@ -2326,6 +2331,9 @@ impl Editor {
     }
 
     pub fn cancel(&mut self, _: &Cancel, cx: &mut ViewContext<Self>) {
+        self.clear_row_highlights::<GitRowHighlight>();
+        let to_remove = self.git_inlays.drain().map(|(id, _)| id).collect();
+        self.splice_inlays(to_remove, Vec::new(), cx);
         if self.dismiss_menus_and_popups(cx) {
             return;
         }
